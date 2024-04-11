e25a7e571e38a6756d73286c976d6fc336854eea
Islamiddin Islamov <islamov.isl168@gmail.com>
2024-04-12 05:23:45 +0600

    updated home_page

--- Frontend/frontend/src/pages/HomePage.js
+++ Frontend/frontend/src/pages/HomePage.js
@@ -48,35 +48,40 @@ function HomePage() {
 
     return (
         <div className="home-page">
-            <header className="home-header">
-                <h1>Онлайн консультация</h1>
+            <header className="home-greeting">
+                <h1 className="home-big-text">Добро пожаловать в</h1>
+                <h1 className="home-big-text-2">MedClose</h1>
+                <p className="home-small-text">Здесь вы можете получить медицинскую консультацию</p>
+                <p className="home-small-text">В удобное для вас время и в любом месте</p>
             </header>
+            <div className="home-box">
 
-            <section className="home-section">
-                <h3>Что вас беспокоит?</h3>
-                <Carousel showThumbs={false} showStatus={false}
-                          showIndicators={true} showArrows={true} emulateTouch={true} infiniteLoop={true}
-                          centerMode={true} centerSlidePercentage={33.33} dynamicHeight={false}>
-                    {problems.map((problem) => (
-                        <div className="home-problem-card" key={problem.slug} onClick={() => handleCardClick(problem)}>
-                            <img src={`http://127.0.0.1:8000${problem.image}`} alt={problem.name}/>
-                            <p>{problem.name}</p>
-                        </div>
-                    ))}
-                </Carousel>
-            </section>
+                <section className="home-section">
+                    <h3>Что вас беспокоит?</h3>
+                    <Carousel showThumbs={false} showStatus={false}
+                              showIndicators={true} showArrows={true} emulateTouch={true} infiniteLoop={true}
+                              centerMode={true} centerSlidePercentage={33.33} dynamicHeight={false}>
+                        {problems.map((problem) => (
+                            <div className="home-problem-card" key={problem.slug} onClick={() => handleCardClick(problem)}>
+                                <img src={`http://127.0.0.1:8000${problem.image}`} alt={problem.name}/>
+                                <p>{problem.name}</p>
+                            </div>
+                        ))}
+                    </Carousel>
+                </section>
 
-            <section className="home-section">
-                <h3>Специалисты</h3>
-                <div className="home-card-container">
-                    {qualifications.map((qualification) => (
-                        <div className="home-card" key={qualification.slug} onClick={() => handleCardClick(qualification)}>
-                            <img src={`http://127.0.0.1:8000${qualification.image}`} alt={qualification.name}/>
-                            <p>{qualification.name}</p>
-                        </div>
-                    ))}
-                </div>
-            </section>
+                <section className="home-section">
+                    <h3>Специалисты</h3>
+                    <div className="home-card-container">
+                        {qualifications.map((qualification) => (
+                            <div className="home-card" key={qualification.slug} onClick={() => handleCardClick(qualification)}>
+                                <img src={`http://127.0.0.1:8000${qualification.image}`} alt={qualification.name}/>
+                                <p>{qualification.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
